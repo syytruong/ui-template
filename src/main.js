04e62c506d73e78eb464ea6588e2e684f85e6aca
syqtr <sy.truong@hoiwa.fi>
2023-08-10 16:27:18 +0300

    add: create router instance in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import UserList from './components/UserList.vue'
+
+// Define routes
+const routes = [
+    {path: '/users/:username', component: UserList}
+];
+
+// Create router instance
+const router = createRouter({
+    history: createWebHistory,
+    routes,
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
